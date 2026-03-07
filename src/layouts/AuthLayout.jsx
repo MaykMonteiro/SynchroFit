82897e0a163272880d35b7153e0f678c408eef48
eliseu <eliseudaniel29@gmail.com>
2026-03-07 02:05:18 -0300

    feat: add icon and logo in the login page

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import img from "../assets/image.png";
 
 export default function AuthLayout() {
   return (
     <div className="min-h-screen w-full flex">
-      <div className="flex-1 bg-sf-bgGray flex items-center justify-center">
-        <div className="text-sf-textWhite font-serif-4 text-8xl tracking-normal">
-          SYNCHRO FIT
-        </div>
+      <div className="flex-1 bg-sf-bgGray">
+        <div
+          className="w-full h-full bg-center bg-no-repeat bg-contain"
+          style={{ backgroundImage: `url(${img})` }}
+        />
       </div>
 
       <div className="w-[420px] bg-sf-greenDark">
-        <div className="w-full px-2 ">
+        <div className="w-full px-2">
           <Outlet />
         </div>
       </div>
